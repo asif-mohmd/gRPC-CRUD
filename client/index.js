cb3a22b130ba5992e6551ba28d00915e04cd63cf
asif-mohmd <asifasifpsps@gmail.com>
2024-03-13 13:43:12 +0530

    express server created

--- client/index.js
+++ client/index.js
@@ -1,1 +1,13 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const PORT  = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log("server running at port ",PORT)
+})
